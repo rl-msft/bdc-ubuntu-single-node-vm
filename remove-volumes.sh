2c369fdfce2587d29d562b991918c385ee7ee511
RL_MSFT <20459971+rl-msft@users.noreply.github.com>
2020-06-17 18:50:55 +0400

    Create remove-volumes.sh

---
+++ remove-volumes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+# num of persistent volumes
+PV_COUNT=50
+
+for i in $(seq 1 $PV_COUNT); do
+  vol="vol$i"
+  sudo umount /mnt/local-storage/$vol || true
+  sudo rm -rf /mnt/local-storage/$vol
+done
