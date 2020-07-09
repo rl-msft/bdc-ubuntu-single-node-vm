c4ed747c1ae59f1163e62cae85d66faa4606e865
RL_MSFT <20459971+rl-msft@users.noreply.github.com>
2020-07-09 21:36:06 +0400

    Update remove-volumes.sh

--- remove-volumes.sh
+++ remove-volumes.sh
@@ -4,7 +4,7 @@
 PV_COUNT=50
 
 for i in $(seq 1 $PV_COUNT); do
-  vol="vol$i"
-  sudo umount /mnt/local-storage/$vol || true
-  sudo rm -rf /mnt/local-storage/$vol
+  vol="vol\$i"
+  sudo umount /mnt/local-storage/\$vol || true
+  sudo rm -rf /mnt/local-storage/\$vol
 done
